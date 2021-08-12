e97ca1dc869c56f45129a165cb258f76457fea48
Pritesh Kumar <pritesh.kumar2@gmail.com>
2021-08-12 19:36:25 +0530

    add springapp context changes

--- src/main/java/com/mavixk/SpringApp.java
+++ src/main/java/com/mavixk/SpringApp.java
@@ -1,5 +1,6 @@
 package com.mavixk;
 
+import com.mavixk.Calculator.AdditionService;
 import com.mavixk.Calculator.MathService;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
@@ -9,7 +10,21 @@ public class SpringApp {
   public static void main(String[] args) {
     ApplicationContext applicationContext = new AnnotationConfigApplicationContext("com.mavixk"
         + ".Calculator");
-    MathService mathService = (MathService) applicationContext.getBean("additionService");
+
+    //log bean names
+    for (String bean : applicationContext.getBeanDefinitionNames()) {
+      System.out.println(bean);
+    }
+
+    MathService mService = applicationContext.getBean(AdditionService.class);
+    mService.operate(7,9);
+
+    //check for singleton instance of additionService bean
+    System.out.println(
+        "additionService is singleton " + applicationContext.isSingleton("additionService"));
+
+            //get the additionsService bean
+            MathService mathService = (MathService) applicationContext.getBean("additionService");
     mathService.operate(3, 4);
   }
 
